faf15c4ad322ebde1cb7ebaaaad086a20c7caafa
Jhonattan Pinzon <pinzonrodriguezjhonattandanilo@gmail.com>
2025-02-09 16:32:45 -0500

    urls

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        # Solo devuelve las tareas del usuario actual
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Asigna el usuario actual a la tarea creada
+        serializer.save(user=self.request.user)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        # Solo devuelve las tareas del usuario actual
+        return Task.objects.filter(user=self.request.user)
+
